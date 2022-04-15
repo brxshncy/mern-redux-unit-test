e62d4f318844c0b8e7e87fc8a04cae4b56c018cd
brxshncy <ggkidsbruce619@gmail.com>
2022-04-15 20:05:04 +0800

    setup routes

---
+++ client/src/config/routesConfig.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Login = React.lazy(() =>
+  import("../containers/Login/Login").then(({ Login }) => ({
+    default: Login,
+  }))
+);
+
+const Register = React.lazy(() =>
+  import("../containers/Register/Register").then(({ Register }) => ({
+    default: Register,
+  }))
+);
+
+const Tweets = React.lazy(() =>
+  import("../containers/Tweets/Tweets").then(({ Tweets }) => ({
+    default: Tweets,
+  }))
+);
+
+const privateRoutes = [
+  {
+    path: "/",
+    element: Tweets,
+  },
+];
